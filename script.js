5e911282896fbcf6e62351e3cd80a861c80d21ad
Vladyslav Hubanov <155400906+vlad-gby@users.noreply.github.com>
2024-02-08 02:06:20 +0200

    Create an average sketchpad, be able to adjust pixed density

--- script.js
+++ script.js
@@ -1,24 +1,99 @@
-const container = document.querySelector('.container');
-
+const scratchpad = document.querySelector('.scratchpad');
+const buttons = document.querySelectorAll('button');
+const contWidth = 800;
+const contHeight = 400;
 
 // CREATE A GRID
-function createGrid(pxlside, width, height){
-  for(let i = 0; i < width * height; i++){
+function createGrid(pxlside){
+  if(document.querySelector('.pixel') !== null){
+    const divs = document.querySelectorAll('.pixel');
+    divs.forEach((e) => {
+      e.remove();
+    });
+  }
+
+  for(let i = 0; i < (contWidth / pxlside) * (contHeight / pxlside); i++){
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
-    container.appendChild(pixel);
+    scratchpad.appendChild(pixel);
   }
-  // SET SIZE
+
   let pixels = document.querySelectorAll('.pixel');
   pixels.forEach((pixel) => {
     pixel.style.width = pxlside + 'px';
     pixel.style.height = pxlside + 'px';
   });
+}
+createGrid(16);
 
-  container.style.width = pxlside * width + 'px';
+// CONTROLS
+buttons.forEach((btn) => {
+  btn.addEventListener('mouseover', e => {
+    btn.style.backgroundColor = 'rgb(48, 119, 10)';
+  });
+  btn.addEventListener('mouseout', e => {
+    btn.style.backgroundColor = 'rgb(39, 98, 7)';
+  });
+  btn.addEventListener('mousedown', e => {
+    btn.style.border = '2px solid rgb(135, 242, 28)';
+  });
+  btn.addEventListener('mouseup', e => {
+    btn.style.border = '2px solid rgb(0,0,0,0)';
+    switch(e.target.id){
+      case '1':
+        createGrid(40);
+      break;
+      case '2':
+        createGrid(36.366);
+      break;
+      case '3':
+        createGrid(16);
+      break;
+      case '4':
+        createGrid(8);
+      break;
+      case '5':
+        createGrid(4);
+        {const pixels = document.querySelectorAll('.pixel');
+        pixels.forEach((pixel) => {
+          pixel.style.border = 'none';
+        });}
+      break;
+      case '6':
+        createGrid(2);
+        {const pixels = document.querySelectorAll('.pixel');
+        pixels.forEach((pixel) => {
+          pixel.style.border = 'none';
+        });}
+      break;
+      case 'clear':
+        const pixels = document.querySelectorAll('.pixel');
+        pixels.forEach((pixel) => {
+          pixel.style.backgroundColor = 'white';
+        });
+      break;
+    }
+  });
+});
+  
+
+function paintHandler(){
+  this.style.backgroundColor = 'rgb(135, 242, 28)';
 }
 
-createGrid(24,10,10);
+scratchpad.addEventListener('mousedown', e => {
+  const pixels = document.querySelectorAll('.pixel');
+
+  pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', paintHandler);
+
+    pixel.addEventListener('mouseup', () => {
+      pixels.forEach((pixel) => {
+        pixel.removeEventListener('mouseover', paintHandler);
+      });
+    });
+  });
+});
 
 
 
